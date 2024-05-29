2307439a764d61c24faeb38237f84d56fac2c797
Yevheniia Kosnevych <tavinka789987@gmail.com>
2024-05-29 17:24:12 +0300

    ++

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-
     const totalPrice = quantity * pricePerDroid;
     if (totalPrice > customerCredits) {
         return "Insufficient funds!";
-        } else {
+    } else {
         return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-        
     }
 }
 
